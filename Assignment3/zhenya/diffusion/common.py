b8632bdb5c385d1e856cd89c8e557d51c589d389
djzhendogg <djzhendogg.is.busy2@gmail.com>
2025-12-15 18:16:42 +0300

    new epoch3

--- Assignment3/zhenya/diffusion/common.py
+++ Assignment3/zhenya/diffusion/common.py
@@ -1,38 +1,50 @@
+import numpy as np
 import torch
-import torchvision.utils as vutils
 from PIL import Image
+from torchvision.utils import save_image
 
 from arh import *
 from unet import UNet
 
+
 def denorm(img):
-    return img.add(1).div(2).clamp(0,1)
+    return img.add(1).div(2).clamp(0, 1)
 
-def save_samples(tensor: torch.Tensor, epoch: int, nrow: int = 4, padding: int = 2):
-    """
-    Сохраняет тензор изображений в виде сетки на локальный диск.
 
-    :param epoch: номер эпохи
-    :param tensor: тензор изображений [B, C, H, W] (на GPU или CPU)
-    :param path: путь для сохранения изображения (например, 'samples/epoch_10.png')
-    :param nrow: количество изображений в ряду сетки
-    :param padding: отступы между изображениями в пикселях
-    :param normalize: если True — нормализует значения в диапазон value_range
-    :param value_range: минимальный и максимальный значения для нормализации (по умолчанию 0–1)
-    """
-    # Переносим на CPU и отвязываем от графов
-    images = denorm(tensor).detach().cpu()
+def save_samples(tensor, epoch: int):
+    fake_images = denorm(tensor)
+    fake_images = fake_images.reshape(fake_images.size(0), 3, 64, 64)
+    fake_fname = './images/fake_images_new-{0:0=4d}.png'.format(epoch)
+    print('Saving', fake_fname)
+    save_image(denorm(fake_images), fake_fname, nrow=4)
 
-    # Создаём сетку (как в torchvision.utils.make_grid)
-    grid = vutils.make_grid(images, nrow=nrow, padding=padding, normalize=False)
 
-    # Преобразуем из тензора [C, H, W] в PIL Image
-    grid_np = grid.permute(1, 2, 0).numpy()  # [H, W, C]
-    # grid_np = (grid_np * 255).astype('uint8')  # в диапазон 0–255
-    image = Image.fromarray(grid_np)
-
-    # Сохраняем на диск
-    image.save(f"./images/fake_image_new_{epoch}.png")
+# def save_samples(tensor: torch.Tensor, epoch: int, nrow: int = 4, padding: int = 2):
+#     """
+#     Сохраняет тензор изображений в виде сетки на локальный диск.
+#
+#     :param epoch: номер эпохи
+#     :param tensor: тензор изображений [B, C, H, W] (на GPU или CPU)
+#     :param path: путь для сохранения изображения (например, 'samples/epoch_10.png')
+#     :param nrow: количество изображений в ряду сетки
+#     :param padding: отступы между изображениями в пикселях
+#     :param normalize: если True — нормализует значения в диапазон value_range
+#     :param value_range: минимальный и максимальный значения для нормализации (по умолчанию 0–1)
+#     """
+#     # Переносим на CPU и отвязываем от графов
+#     images = denorm(tensor).detach().cpu()
+#
+#     # Создаём сетку (как в torchvision.utils.make_grid)
+#     grid = vutils.make_grid(images, nrow=nrow, padding=padding, normalize=False)
+#
+#     # Преобразуем из тензора [C, H, W] в PIL Image
+#     grid_np = grid.permute(1, 2, 0).numpy()  # [H, W, C]
+#     # grid_np = (grid_np * 255).astype('uint8')  # в диапазон 0–255
+#     grid_np = grid_np.astype('uint8')
+#     image = Image.fromarray(grid_np)
+#
+#     # Сохраняем на диск
+#     image.save(f"./images/fake_image_new_{epoch}.png")
 
 
 def save_model(configs, path: str):
@@ -42,15 +54,10 @@ def save_model(configs, path: str):
     :param configs: экземпляр Configs (с обученной моделью)
     :param path: путь к файлу (.pth или .pt)
     """
-    torch.save({
-        'model_state_dict': configs.eps_model.state_dict(),
-        'optimizer_state_dict': configs.optimizer.state_dict(),
-        'n_steps': configs.n_steps,
-        'image_channels': configs.image_channels,
-        'n_channels': configs.n_channels,
-        'channel_multipliers': configs.channel_multipliers,
-        'is_attention': configs.is_attention,
-    }, path)
+    torch.save(
+        {'model_state_dict': configs.eps_model.state_dict(), 'optimizer_state_dict': configs.optimizer.state_dict(),
+            'n_steps': configs.n_steps, 'image_channels': configs.image_channels, 'n_channels': configs.n_channels,
+            'channel_multipliers': configs.channel_multipliers, 'is_attention': configs.is_attention, }, path)
 
 
 def load_model(path: str, device: torch.device) -> DenoiseDiffusion:
@@ -64,21 +71,13 @@ def load_model(path: str, device: torch.device) -> DenoiseDiffusion:
     checkpoint = torch.load(path, map_location=device)
 
     # Восстанавливаем UNet
-    model = UNet(
-        image_channels=checkpoint['image_channels'],
-        n_channels=checkpoint['n_channels'],
-        ch_mults=checkpoint['channel_multipliers'],
-        is_attn=checkpoint['is_attention']
-    ).to(device)
+    model = UNet(image_channels=checkpoint['image_channels'], n_channels=checkpoint['n_channels'],
+        ch_mults=checkpoint['channel_multipliers'], is_attn=checkpoint['is_attention']).to(device)
 
     # Загружаем веса
     model.load_state_dict(checkpoint['model_state_dict'])
 
     # Создаём DenoiseDiffusion
-    diffusion = DenoiseDiffusion(
-        eps_model=model,
-        n_steps=checkpoint['n_steps'],
-        device=device
-    )
+    diffusion = DenoiseDiffusion(eps_model=model, n_steps=checkpoint['n_steps'], device=device)
 
     return diffusion
