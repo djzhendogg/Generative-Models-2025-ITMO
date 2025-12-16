d901e0fb14532480ae228447967b34146a05efe7
djzhendogg <djzhendogg.is.busy2@gmail.com>
2025-12-16 17:16:50 +0300

    add metric counter1

--- Assignment3/count_metric.py
+++ Assignment3/count_metric.py
@@ -6,12 +6,12 @@ from pytorch_image_generation_metrics import (
 import torch
 from pytorch_image_generation_metrics import ImageDataset
 from torch.utils.data import DataLoader, Dataset
-device = "cpu"
+device = "cuda"
 npz_path = "/mnt/tank/scratch/edin/Generative-Models-2025-ITMO/Assignment3/zhenya/fid_stats_cifar10_train.npz"
 generatore_path = "/mnt/tank/scratch/edin/Generative-Models-2025-ITMO/Assignment3/zhenya/generator.pkl"
-G_loaded = torch.load(generatore_path, weights_only=False, map_location=torch.device('cpu'))
+G_loaded = torch.load(generatore_path, weights_only=False, map_location=torch.device('cuda'))
 G_loaded.eval()
-
+print("Model Loaded")
 # create the Generator Dataset. returns the image, generated from the Generator network
 class GeneratorDataset(Dataset):
     def __init__(self, G, noise_dim):
@@ -35,6 +35,7 @@ loader = DataLoader(dataset, batch_size=50, num_workers=0)
 # FID = get_fid(
 #     loader, npz_path)
 # Inception Score + Frechet Inception Distance
+print("Count metrics...")
 (IS, IS_std), FID = get_inception_score_and_fid(
     loader, npz_path)
 print("IS: %.3f, IS_std: %.3f, FID: %.3f" % (IS, IS_std, FID))
